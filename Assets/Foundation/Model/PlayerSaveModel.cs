d5a6531d847c9b1fdcd0288fbeff325a6b906d4d
Eugene Kim <komastar.dev@gmail.com>
2021-01-04 14:56:20 +0900

    PlayerSaveModel 추가

---
+++ Assets/Foundation/Model/PlayerSaveModel.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+
+namespace Assets.Foundation.Model
+{
+    public class PlayerSaveModel
+    {
+        public int Version { get; set; }
+        public List<StageClearModel> ClearStages { get; set; }
+
+        public PlayerSaveModel()
+        {
+            ClearStages = new List<StageClearModel>();
+        }
+
+        public PlayerSaveModel(int version)
+        {
+            Version = version;
+            ClearStages = new List<StageClearModel>();
+        }
+
+        public static PlayerSaveModel MakeNewPlayerData()
+        {
+            return new PlayerSaveModel(1);
+        }
+
+        public bool IsClear(int stageId)
+        {
+            var find = ClearStages.Find(s => s.Id == stageId);
+            if (null == find)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public void ClearStage(int stageId)
+        {
+            if (IsClear(stageId))
+            {
+                ClearStages.Add(new StageClearModel() { Id = stageId });
+            }
+        }
+    }
+
+    public class StageClearModel
+    {
+        public int Id { get; set; }
+    }
+}
