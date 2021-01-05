9c6bbbcecf25e0a814b98967ac12de2c2d7ec276
Eugene Kim <komastar.dev@gmail.com>
2021-01-05 11:15:17 +0900

    v1.7.2 close #16 로비씬 게임 설명서 수정. 점수 계산 설명서를 점수창에서 볼 수 있게 수정

---
+++ Assets/Foundation/UI/Common/UICllickClose.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Assets.Foundation.UI.Common
+{
+    public class UICllickClose : MonoBehaviour, IPointerClickHandler
+    {
+        public void Open()
+        {
+            gameObject.SetActive(true);
+        }
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
