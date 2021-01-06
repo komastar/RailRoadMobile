3ef4eeaaa7683d0ba0c46878057c3fb07bfb3650
Eugene Kim <komastar.dev@gmail.com>
2021-01-06 14:42:45 +0900

    v1.8.0 close #18 혼자하기 챕터와 스테이지 선택 화면 개선

--- Assets/UI/Lobby/UISoloPlayPanel.cs
+++ Assets/UI/Lobby/UISoloPlayPanel.cs
@@ -14,35 +14,73 @@ namespace Assets.UI.Lobby
         private DataManager dataManager;
 
         public UIStageButton stageButtonPrefab;
-        public Transform stagePanel;
         public GameObject loadingPanel;
 
+        private List<Button> stageButtonList;
         private Dictionary<int, UIStageButton> stageButtons;
+
+        public Text chapterNameText;
+
+        public Button chapterLeftButton;
+        public Button chapterRightButton;
+
+        public Transform chapterPages;
+        public RectTransform chapterPagesRect;
+
+        public UIChapterPage chapterPagePrefab;
+        public List<UIChapterPage> chapterPageList;
+
+        public float pagingSpeed;
+        public int chapterIndex;
+        public int chapterCount;
+        public float chapterPageWidth;
+
+        private Vector2 pageDestination;
+
+        private void Update()
+        {
+            chapterPagesRect.anchoredPosition = Vector2.Lerp(chapterPagesRect.anchoredPosition, pageDestination, pagingSpeed * Time.deltaTime);
+        }
 
         public override void Setup()
         {
             gameManager = GameManager.Get();
             dataManager = DataManager.Get();
-            stageButtons = new Dictionary<int, UIStageButton>();
             gameManager.GameRoom = GameRoomModel.GetSoloPlay();
 
-            var stages = dataManager.StageListData;
-            buttons = new Button[stages.Count];
-            int buttonIndex = 0;
-            foreach (var stage in stages)
+            pageDestination = chapterPagesRect.anchoredPosition;
+            chapterPageWidth = chapterPagePrefab.GetComponent<RectTransform>().rect.width;
+
+            stageButtonList = new List<Button>();
+            stageButtons = new Dictionary<int, UIStageButton>();
+            chapterPageList = new List<UIChapterPage>();
+
+            var chapters = dataManager.ChapterData;
+            chapterIndex = gameManager.GetLastChapterIndex();
+            chapterCount = chapters.Count;
+            foreach (var chapter in chapters)
             {
-                var button = Instantiate(stageButtonPrefab, stagePanel);
-                button.StageData = stage.Value.Stage;
-                button.SetStageNumber(buttonIndex + 1);
-                button.SetStageStatus(false);
-                buttons[buttonIndex] = button.ownButton;
-                stageButtons.Add(stage.Value.Id, button);
-                buttonIndex++;
+                var chapterPage = Instantiate(chapterPagePrefab, chapterPages);
+                chapterPage.SetName(chapter.Value.Name);
+                chapterPageList.Add(chapterPage);
+                int stageIndex = 0;
+                foreach (var stage in chapter.Value.Stages)
+                {
+                    var stageButton = Instantiate(stageButtonPrefab, chapterPage.transform);                    
+                    stageButton.StageData = dataManager.StageListData[stage].Stage;
+                    stageButton.SetStageNumber(stageIndex + 1);
+                    stageButton.SetStageStatus(false);
+                    stageButtonList.Add(stageButton.ownButton);
+                    stageButtons.Add(stage, stageButton);
+                    stageIndex++;
+                }
             }
 
             onBeforeOpen = null;
             onBeforeOpen += async () =>
             {
+                UpdateChapterPage(true);
+
                 bool isClear = true;
                 foreach (var button in stageButtons)
                 {
@@ -51,11 +89,12 @@ namespace Assets.UI.Lobby
                     button.Value.SetStageStatus(isClear);
                 }
 
-                var clicked = await UIButtonAsync.SelectButton<UIStageButton>(buttons);
+                var clicked = await UIButtonAsync.SelectButton<UIStageButton>(stageButtonList.ToArray());
                 Close();
                 loadingPanel.SetActive(true);
                 gameManager.CurrentChapter = dataManager.GetFirstChapter();
                 gameManager.CurrentStage = clicked.StageData;
+                gameManager.SetLastChapterIndex(chapterIndex);
                 var loadScene = SceneManager.LoadSceneAsync("GameScene");
                 loadScene.completed += (op) =>
                 {
@@ -67,6 +106,37 @@ namespace Assets.UI.Lobby
         public override void Close()
         {
             base.Close();
+        }
+
+        public void OnClickNextButton(bool isRight)
+        {
+            if (isRight)
+            {
+                chapterIndex++;
+            }
+            else
+            {
+                chapterIndex--;
+            }
+
+            UpdateChapterPage();
+        }
+
+        private void UpdateChapterPage(bool isInstant = false)
+        {
+            chapterIndex = Mathf.Clamp(chapterIndex, 0, chapterCount - 1);
+            if (isInstant)
+            {
+                chapterPagesRect.anchoredPosition = new Vector3(-chapterPageWidth * chapterIndex, 0f);
+                pageDestination = chapterPagesRect.anchoredPosition;
+            }
+            else
+            {
+                pageDestination = new Vector3(-chapterPageWidth * chapterIndex, 0f);
+            }
+            chapterNameText.text = chapterPageList[chapterIndex].Name;
+            chapterLeftButton.interactable = chapterIndex > 0;
+            chapterRightButton.interactable = chapterIndex < chapterCount - 1;
         }
     }
 }
